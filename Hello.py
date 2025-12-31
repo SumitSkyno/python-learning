d9a630004b2499a9c841e39059b2fd72c96f67e5
SumitSkyno <sumit@skynodigital.com>
2025-12-31 15:10:49 +0530

    Added first Python program (Hello World)

---
+++ Hello.py
@@ -0,0 +1,11 @@
+print ("Hello Wolrd!")
+
+
+#Text in Python must be inside quotes. You can use either " double quotes or ' single quotes
+print('Single quotes statement')
+
+
+#you want to print multiple words on the same line, you can use the end parameter: with comma in between 
+print ("Same line: ", end="")
+print ("This will print in same line")
+
